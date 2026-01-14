07e9720c4af397698a3e8444a1a57e379605fe28
aracelyvi <avidrios2@ewu.edu>
2026-01-14 03:10:47 +0000

    Add initial project files including HTML, CSS, and JavaScript; implement basic structure and styling

--- scripts/main.js
+++ scripts/main.js
@@ -6,10 +6,17 @@
 // as you learn to make the web behave.
 // =============================================
 
+// Store a reference to the <h1> in a variable
+const myHeading = document.querySelector("h1");
+// Update the text content of the <h1>
+myHeading.textContent = "Hello world!";
+
 console.log('Portfolio loaded');
 
 // Week 0: Hello World!
 alert("Hello World!" );
+
+
 
 // Week 1: You'll add theme toggle code here
 // Week 2: You'll add localStorage persistence here
