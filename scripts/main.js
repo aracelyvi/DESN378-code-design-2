f77e247a692dd4b08880f66f978f3d58a9aedc6c
aracelyvi <avidrios2@ewu.edu>
2026-01-14 04:54:34 +0000

    Refactor main.js to streamline image toggle functionality; add new firefox2 image

--- scripts/main.js
+++ scripts/main.js
@@ -7,11 +7,16 @@
 // =============================================
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Store a reference to the <h1> in a variable
-  const myHeading = document.querySelector('h1');
-  // Update the text content of the <h1>
-  if (myHeading) myHeading.textContent = 'Test Heading!';
+ const myImage = document.querySelector("img");
 
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+});
   console.log('Portfolio loaded');
 
   // Week 0: Hello World!
