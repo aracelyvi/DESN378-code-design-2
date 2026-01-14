bedcef29a758b9f50214de40179b30c57b74773f
aracelyvi <avidrios2@ewu.edu>
2026-01-14 05:14:04 +0000

    Refactor main.js to consolidate setUserName function and improve user name handling logic

--- scripts/main.js
+++ scripts/main.js
@@ -17,10 +17,38 @@ myImage.addEventListener("click", () => {
     myImage.setAttribute("src", "images/firefox-icon.png");
     
   }
- 
-
-
 });
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+myButton.addEventListener("click", () => {
+  setUserName();
+});
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+
+
+
   console.log('Portfolio loaded');
 
   // Week 0: Hello World!
